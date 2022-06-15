c92445a9b9ca0d5751e5163f8fac69e87631a91a
James Hassett <jhassett189@gmail.com>
2022-06-15 19:27:16 -0400

    commit tree a1cd5016bb0e87f48d58db2e71509cfed5d86715

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name) {
+    const card = []
+    for (let i = 0; i < name.length; i++) {
+        card.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return card;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number)
+        number--;
+    }
+    console.log(number)
+}
